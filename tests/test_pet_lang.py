6a2560e2f2db894c200eaf017bdcfe1a705d131f
Henry LETELLIER <henry.letellier@epitech.eu>
2024-04-03 03:36:09 +0200

    [UPDATE] unit tests

--- tests/test_pet_lang.py
+++ tests/test_pet_lang.py
@@ -1,7 +1,7 @@
 # tests/test_ask_question.py
 import unittest.mock
 from sys import stderr
-from ask_question import AskQuestion
+from pet_lang import PetLang
 
 
 def print_debug(string: str = "") -> None:
@@ -13,7 +13,7 @@ def print_debug(string: str = "") -> None:
 
 def test_is_empty() -> None:
     """ Test if the input string is empty """
-    aqi = AskQuestion()
+    aqi = PetLang()
     response = aqi.is_empty("")
     print_debug(f"response = {response}")
     response2 = aqi.is_empty("\t")
@@ -30,7 +30,7 @@ def test_is_empty() -> None:
 
 def test_is_version() -> None:
     """ Test if the version tester is working """
-    aqi = AskQuestion()
+    aqi = PetLang()
     response = aqi.is_version("1.0.0")
     print_debug(f"response = {response}")
     response2 = aqi.is_version("1.a.0")
@@ -41,7 +41,7 @@ def test_is_version() -> None:
 
 def test_contains_illegal_characters() -> None:
     """ Test if the illegal characters tester is working """
-    aqi = AskQuestion()
+    aqi = PetLang()
     illegal_chars = "#@$"
     response = aqi.contains_illegal_characters("Hello", illegal_chars)
     print_debug(f"response = {response}")
@@ -53,7 +53,7 @@ def test_contains_illegal_characters() -> None:
 
 def test_remove_char_overflow() -> None:
     """ Test if the character overflow remover is working """
-    aqi = AskQuestion()
+    aqi = PetLang()
     response = aqi.remove_char_overflow("Hello", "l", 1, False)
     print_debug(f"response = {response}")
     response2 = aqi.remove_char_overflow("Hello", "l", 2, False)
@@ -64,7 +64,7 @@ def test_remove_char_overflow() -> None:
 
 def test_clean_number() -> None:
     """ Test if the number cleaner is working """
-    aqi = AskQuestion()
+    aqi = PetLang()
     response = aqi.clean_number("1,000.99")
     print_debug(f"response = {response}")
     response2 = aqi.clean_number("1.234.567,89")
@@ -77,7 +77,7 @@ def test_clean_number() -> None:
 # Simulate user input for "1.23" when asked for a float
 def test_ask_question_valid_input(mock_input) -> None:
     """Test asking question with valid inputs."""
-    aqi = AskQuestion()
+    aqi = PetLang()
     response = aqi.ask_question("Enter a float: ", "float")
     print_debug(f"response = {response}")
     assert response == 1.23
@@ -85,7 +85,7 @@ def test_ask_question_valid_input(mock_input) -> None:
 
 def test_ask_question_invalid_input() -> None:
     """Test asking question with invalid inputs."""
-    aqi = AskQuestion()
+    aqi = PetLang()
     response = aqi.test_input("abc ", "int")
     print_debug(f"response = {response}")
     response2 = aqi.usr_answer
@@ -98,7 +98,7 @@ def test_ask_question_invalid_input() -> None:
 # Simulate user input for "y" when asked for a bool
 def test_ask_question_bool_input(mock_input):
     """Test asking boolean questions and answering them correctly."""
-    aqi = AskQuestion()
+    aqi = PetLang()
     response = aqi.ask_question("Yes or No? ", "bool")
     print_debug(f"response = {response}")
     assert response is True
@@ -108,7 +108,7 @@ def test_ask_question_bool_input(mock_input):
 # Simulate user input for "1.0.0" when asked for a version
 def test_ask_question_version_input(mock_input):
     """Test asking version questions and answering them correctly."""
-    aqi = AskQuestion()
+    aqi = PetLang()
     response = aqi.ask_question("Enter a version: ", "version")
     print_debug(f"response = {response}")
     assert response == "1.0.0"
@@ -116,7 +116,7 @@ def test_ask_question_version_input(mock_input):
 
 def test_ask_question_empty_input():
     """Test asking empty string as input to ask_question function."""
-    aqi = AskQuestion()
+    aqi = PetLang()
     response = aqi.test_input("", "str")
     print_debug(f"response = {response}")
     response2 = aqi.usr_answer
@@ -127,7 +127,7 @@ def test_ask_question_empty_input():
 
 def test_ask_question_space_input():
     """Test asking empty string as input to ask_question function."""
-    aqi = AskQuestion()
+    aqi = PetLang()
     response = aqi.test_input(" ", "str")
     print_debug(f"response = {response}")
     response2 = aqi.usr_answer
@@ -138,7 +138,7 @@ def test_ask_question_space_input():
 
 def test_ask_question_tab_input():
     """Test asking empty string as input to ask_question function."""
-    aqi = AskQuestion()
+    aqi = PetLang()
     response = aqi.test_input("\t", "str")
     print_debug(f"response = {response}")
     response2 = aqi.usr_answer
@@ -149,7 +149,7 @@ def test_ask_question_tab_input():
 
 def test_ask_question_tab_and_space_input():
     """Test asking empty string as input to ask_question function."""
-    aqi = AskQuestion()
+    aqi = PetLang()
     response = aqi.test_input("\t ", "str")
     print_debug(f"response = {response}")
     response2 = aqi.usr_answer
@@ -160,7 +160,7 @@ def test_ask_question_tab_and_space_input():
 
 def test_ask_question_tab_and_space_mix_input():
     """Test asking empty string as input to ask_question function."""
-    aqi = AskQuestion()
+    aqi = PetLang()
     response = aqi.test_input(
         "\t  \t\t\t  \t\t   \t\t  \t\t   \t\t \t\t",
         "str"
@@ -174,7 +174,7 @@ def test_ask_question_tab_and_space_mix_input():
 
 def test_ask_question_whitespace_input():
     """Test asking whitespace as input to ask_question function."""
-    aqi = AskQuestion()
+    aqi = PetLang()
     # Simulate user input for whitespace
     response = aqi.test_input("            ", "str")
     print_debug(f"response = {response}")
@@ -186,7 +186,7 @@ def test_ask_question_whitespace_input():
 
 def test_ask_question_tabs_input():
     """Test asking whitespace as input to ask_question function."""
-    aqi = AskQuestion()
+    aqi = PetLang()
     # Simulate user input for tabs
     response = aqi.test_input("\t\t\t\t\t\t\t\t\t\t\t\t", "str")
     print_debug(f"response = {response}")
@@ -198,7 +198,7 @@ def test_ask_question_tabs_input():
 
 def test_ask_question_invalid_characters():
     """Test asking invalid characters as input to ask_question function."""
-    aqi = AskQuestion()
+    aqi = PetLang()
     # Simulate user input for "!@#" when asked for alphanumeric
     response = aqi.test_input("!@#", "alnum")
     print_debug(f"response = {response}")
@@ -212,7 +212,7 @@ def test_ask_question_invalid_characters():
 # Simulate user input for "42" when asked for a uint
 def test_ask_question_valid_uint(mock_input):
     """ Test asking valid uints (positive integers) in the range [0, 2^32-1]. """
-    aqi = AskQuestion()
+    aqi = PetLang()
     response = aqi.ask_question("Enter a uint: ", "uint")
     print_debug(f"response = {response}")
     assert response == 42
@@ -220,7 +220,7 @@ def test_ask_question_valid_uint(mock_input):
 
 def test_ask_question_invalid_uint():
     """Test asking invalid uints (negative integers) in the range [0, 2^32-1]."""
-    aqi = AskQuestion()
+    aqi = PetLang()
     # Simulate user input for "-42" when asked for a uint
     response = aqi.test_input("-42", "uint")
     print_debug(f"response = {response}")
@@ -234,7 +234,7 @@ def test_ask_question_invalid_uint():
 # Simulate user input for "3.14" when asked for a ufloat
 def test_ask_question_valid_ufloat(mok_input):
     """Test asking valid ufloats (positive floats) in the range [0, 2^32-1]."""
-    aqi = AskQuestion()
+    aqi = PetLang()
     response = aqi.ask_question("Enter a ufloat: ", "ufloat")
     print_debug(f"response = {response}")
     assert response == 3.14
@@ -242,7 +242,7 @@ def test_ask_question_valid_ufloat(mok_input):
 
 def test_ask_question_invalid_ufloat():
     """Test asking invalid ufloats (negative floats) in the range [0, 2^32-1]."""
-    aqi = AskQuestion()
+    aqi = PetLang()
     # Simulate user input for "-3.14" when asked for a ufloat
     response = aqi.test_input("-3.14", "ufloat")
     print_debug(f"response = {response}")
